af0dec108b7a584be1f7ba80c799f43419427438
Kristofer <kristofer@fmckl.se>
2024-10-23 14:47:56 +0200

    Badges

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,4 +1,4 @@
-import Login from './components/login'
+import Login from './components/login';
 import { useInternetIdentity } from 'ic-use-internet-identity';
 import Wallet from './components/wallet';
 import { Toaster } from './components/ui/toaster';
@@ -7,34 +7,41 @@ function AppInner() {
   const { identity } = useInternetIdentity();
 
   if (!identity) {
-    return <Login />
+    return <Login />;
   }
 
-  return (
-    <Wallet />
-  )
+  return <Wallet />;
 }
 
 export default function App() {
-  return <main>
-    <AppInner />
-    <Toaster />
+  return (
+    <main>
+      <AppInner />
+      <Toaster />
 
-    <div className="links">
-      <a
-        href="https://github.com/ic-alloy/ic-alloy-basic-wallet"
-        target="_blank" rel="noreferrer"
-      >
-        <img src="https://img.shields.io/github/license/ic-alloy/ic-alloy-basic-wallet" />
-      </a>
-
-      <a
-        href="https://github.com/ic-alloy/ic-alloy-basic-wallet"
-        target="_blank" rel="noreferrer"
-      >
-        <img src="https://img.shields.io/github/stars/ic-alloy/ic-alloy-basic-wallet" />
-      </a>
-    </div>
-  </main>
+      <div className="links">
+        <a
+          href="https://github.com/ic-alloy/ic-alloy-basic-wallet/graphs/contributors"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="https://img.shields.io/github/contributors/ic-alloy/ic-alloy-toolkit.svg?style=for-the-badge" />
+        </a>
+        <a
+          href="https://github.com/ic-alloy/ic-alloy-basic-wallet"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="https://img.shields.io/github/license/ic-alloy/ic-alloy-toolkit.svg?style=for-the-badge" />
+        </a>
+        <a
+          href="https://github.com/ic-alloy/ic-alloy-basic-wallet/stargazers"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="https://img.shields.io/github/stars/ic-alloy/ic-alloy-toolkit?style=for-the-badge" />
+        </a>
+      </div>
+    </main>
+  );
 }
-
